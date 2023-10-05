b506f8072e7f8ae7f3d61374c20eca5139f2ff64
mahmoudelgharabawi <m.elgharabawi@tecfy.co>
2023-10-05 09:33:20 +0300

    update

--- lib/widgets/file_view_widget.dart
+++ lib/widgets/file_view_widget.dart
@@ -21,24 +21,24 @@ class FileViewWidget extends StatefulWidget {
 }
 
 class _FileViewWidgetState extends State<FileViewWidget> {
-  Future<String?> getDownloadPath() async {
-    Directory? directory;
-    try {
-      if (!Platform.isAndroid) {
-        directory = await getApplicationDocumentsDirectory();
-      } else {
-        directory = Directory('/storage/emulated/0/Download');
-        // Put file in global download folder, if for an unknown reason it didn't exist, we fallback
-        // ignore: avoid_slow_async_io
-        if (!await directory.exists()) {
-          directory = await getExternalStorageDirectory();
-        }
-      }
-    } catch (err) {
-      print("Cannot get download folder path");
-    }
-    return directory?.path;
-  }
+  // Future<String?> getDownloadPath() async {
+  //   Directory? directory;
+  //   try {
+  //     if (!Platform.isAndroid) {
+  //       directory = await getApplicationDocumentsDirectory();
+  //     } else {
+  //       directory = Directory('/storage/emulated/0/Download');
+  //       // Put file in global download folder, if for an unknown reason it didn't exist, we fallback
+  //       // ignore: avoid_slow_async_io
+  //       if (!await directory.exists()) {
+  //         directory = await getExternalStorageDirectory();
+  //       }
+  //     }
+  //   } catch (err) {
+  //     print("Cannot get download folder path");
+  //   }
+  //   return directory?.path;
+  // }
 
   void getStoragePermission() async {
     try {

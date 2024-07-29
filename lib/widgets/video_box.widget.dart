d3a5b6a1aaaf36189ff84e03e33aca2b690dfe9b
mahmoudelgharabawi <m.elgharabawi@tecfy.co>
2024-07-29 16:38:30 +0300

    fix video issue

--- lib/widgets/video_box.widget.dart
+++ lib/widgets/video_box.widget.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:video_box/video_box.dart';
 
 class VideoBoxWidget extends StatefulWidget {
@@ -14,19 +19,47 @@ class _VideoBoxWidgetState extends State<VideoBoxWidget> {
 
   @override
   void initState() {
-    vc = VideoController(
-        source: VideoPlayerController.networkUrl(Uri.parse(widget.url)))
-      ..addInitializeErrorListenner((e) {
-        print('[video box init] error: ' + e.message);
-      })
-      ..initialize().then((e) {
-        if (e != null) {
-          print('[video box init] error: ' + e.message);
-        } else {
-          print('[video box init] success');
-        }
-      });
     super.initState();
+  }
+
+  void init() async {
+    if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
+      var path =
+          '${(await getTemporaryDirectory()).path}/${widget.url.split('files').last.replaceAll('/', '-')}.mp4';
+      var file = File(path);
+      if (!(await file.exists())) {
+        var bytes =
+            (await NetworkAssetBundle(Uri.parse(widget.url)).load(widget.url))
+                .buffer
+                .asUint8List();
+        await file.writeAsBytes(bytes);
+      }
+
+      vc = VideoController(source: VideoPlayerController.file(file))
+        ..addInitializeErrorListenner((e) {
+          print('[video box init] error: ' + e.message);
+        })
+        ..initialize().then((e) {
+          if (e != null) {
+            print('[video box init] error: ' + e.message);
+          } else {
+            print('[video box init] success');
+          }
+        });
+    } else {
+      vc = VideoController(
+          source: VideoPlayerController.networkUrl(Uri.parse(widget.url)))
+        ..addInitializeErrorListenner((e) {
+          print('[video box init] error: ' + e.message);
+        })
+        ..initialize().then((e) {
+          if (e != null) {
+            print('[video box init] error: ' + e.message);
+          } else {
+            print('[video box init] success');
+          }
+        });
+    }
   }
 
   @override
